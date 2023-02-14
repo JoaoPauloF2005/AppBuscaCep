1de5f0105932f276b181c51eb62cf9ecbad64b7d
Usuario <Usuario@CPD03-M03>
2023-02-14 15:01:39 -0300

    Adicionar arquivos de projeto.

---
+++ AppBuscaCep/AppBuscaCep/Service/DataService.cs
@@ -0,0 +1,28 @@
+﻿using AppBuscaCep.Model;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscaCep.Service
+{
+    public class DataService
+    {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+
+            using (HttpClient cliente = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
